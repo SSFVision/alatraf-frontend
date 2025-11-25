578f82254700e7679a1cad25fc680261f9793990
WaleedAwn <waleed.awn2023@gmail.com>
2025-11-25 21:56:00 +0300

    adding the auth models

---
+++ Alatraf.Managment.system/src/app/core/auth/models/user-session.model.ts
@@ -0,0 +1,11 @@
+export interface UserSession {
+  user: UserModel | null;
+
+  accessToken: string | null;
+  refreshToken: string | null;
+  expiresOnUtc: string | null;
+
+  tokenType: string | null;  // "Bearer"
+
+  isLoggedIn: boolean;
+}
