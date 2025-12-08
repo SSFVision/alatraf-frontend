1c83a9c21aa203a4067aea4d3279b4aa95ade37f
WaleedAwn <waleed.awn2023@gmail.com>
2025-12-08 22:03:31 +0300

    update pagetion

--- Alatraf.Managment.system/src/app/features/Diagnosis/Therapy/Pages/therapy-waiting-list/therapy-waiting-list.component.ts
+++ Alatraf.Managment.system/src/app/features/Diagnosis/Therapy/Pages/therapy-waiting-list/therapy-waiting-list.component.ts
@@ -1,41 +1,8 @@
-// import { Component, inject, signal, OnInit } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-// import { PatientCardComponent } from '../../../Shared/Components/waiting-patient-card/waiting-patient-card.component';
-// import { Patient } from '../../../../Reception/Patients/models/patient.model';
-// import { PatientsFacade } from '../../../../Reception/Patients/Services/patients.facade.service';
-// import { NavigationDiagnosisFacade } from '../../../../../core/navigation/navigation-diagnosis.facade';
-
-// @Component({
-//   selector: 'app-therapy-waiting-list',
-//   imports: [RouterOutlet, PatientCardComponent],
-//   templateUrl: './therapy-waiting-list.component.html',
-//   styleUrl: './therapy-waiting-list.component.css',
-// })
-// export class TherapyWaitingListComponent implements OnInit {
-//   selectedPatient = signal<Patient |null>(null);
-//   private facade = inject(PatientsFacade);
-//   patients = this.facade.patients;
-//   private navDiagnos = inject(NavigationDiagnosisFacade);
-//   ngOnInit() {
-//     this.facade.loadPatients();
-//   }
-//  onSearch(term: string) {
-//     this.facade.search(term);
-//   }
-
-//   selectPatient(patient: Patient) {
-//     this.selectedPatient.set(patient);
-//     this.navDiagnos.goToTherapyCreate(patient.patientId);
-//   }
-// }
-
 import { Component, inject, signal, OnInit, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PatientCardComponent } from '../../../Shared/Components/waiting-patient-card/waiting-patient-card.component';
 import { NavigationDiagnosisFacade } from '../../../../../core/navigation/navigation-diagnosis.facade';
-import { PatientDto } from '../../../../../core/models/Shared/patient.model';
 import { TicketDto } from '../../../../Reception/Tickets/models/ticket.model';
-import { TicketService } from '../../../../Reception/Tickets/ticket.service';
 import { TicketFacade } from '../../../../Reception/Tickets/tickets.facade.service';
 import { PaginationComponent } from "../../../../../shared/components/pagination/pagination.component";
 
@@ -56,7 +23,6 @@ export class TherapyWaitingListComponent implements OnInit {
   selectedTicket = signal<TicketDto | null>(null);
 
   ngOnInit() {
-    // load first page
     this.ticketFacade.loadTickets();
   }
 
@@ -64,24 +30,14 @@ export class TherapyWaitingListComponent implements OnInit {
     this.ticketFacade.search(term);
   }
 
-  // filterByDepartment(departmentId: number | null) {
-  //   this.ticketFacade.updateFilters({
-  //     departmentId: departmentId ?? undefined,
-  //   });
-
-  //   // reset page and reload
-  //   this.ticketFacade.setPage(1);
-  // }
+ 
 filterByDepartment(departmentId: number | null) {
-  // update active button
   this.activeDepartment.set(departmentId);
 
-  // update filters in facade
   this.ticketFacade.updateFilters({
     departmentId: departmentId ?? undefined,
   });
 
-  // reset to first page & load
   this.ticketFacade.setPage(1);
 }
 
@@ -90,7 +46,6 @@ filterByDepartment(departmentId: number | null) {
     this.navDiagnos.goToTherapyCreate(ticket.patient?.patientId!);
   }
 
-  // expose page data for template (optional)
   pageRequest = this.ticketFacade.pageRequest;
   totalCount = this.ticketFacade.totalCount;
 }
