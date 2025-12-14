8b1aeea350a984980ba51018ab7cc84500f4e1a8
WaleedAwn <waleed.awn2023@gmail.com>
2025-12-14 20:39:07 +0300

    add the orgnize and remove unused imprts for the ts fiel

--- Alatraf.Managment.system/src/app/features/Reception/Patients/components/patient-summary-card/patient-summary-card.component.ts
+++ Alatraf.Managment.system/src/app/features/Reception/Patients/components/patient-summary-card/patient-summary-card.component.ts
@@ -1,10 +1,14 @@
-import { Component, inject, input, Input, output, signal } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import { NavigationReceptionFacade } from '../../../../../core/navigation/navigation-reception.facade';
+import { Component, inject, input, output } from '@angular/core';
+
 import { PatientDto } from '../../../../../core/models/Shared/patient.model';
+import {
+  NavigationReceptionFacade
+} from '../../../../../core/navigation/navigation-reception.facade';
 
 @Component({
   selector: 'app-patient-summary-card',
+  standalone: true,
   imports: [DatePipe],
   templateUrl: './patient-summary-card.component.html',
   styleUrl: './patient-summary-card.component.css',
