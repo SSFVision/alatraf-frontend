7f4f83647d49407bd5734c40b40849ae032e4eef
Youssef Al-Hammadi <youssefesmailalhammadi90@gmail.com>
2026-01-06 11:41:43 +0300

    load icons and add them to side bar :>

--- Alatraf.Managment.system/src/app/core/navigation/sidebar.items.ts
+++ Alatraf.Managment.system/src/app/core/navigation/sidebar.items.ts
@@ -21,20 +21,20 @@ export const MENU_CONFIG: MenuCategory[] = [
     items: [
       {
         label: ' المستخدمين',
-        icon: 'assets/icons/user-icon-w.svg',
-        activeIcon: 'assets/icons/user-icon-b.svg',
+        icon: 'assets/icons/user-management-w.svg',
+        activeIcon: 'assets/icons/user-management-b.svg',
         route: AppRoutes.users.root,
         requiredPermissions: [PERMISSIONS.PAYMENTS.VIEW],
       },
       {
-        label: 'إدارةالدكاترة',
-        icon: 'assets/icons/user-icon-w.svg',
-        activeIcon: 'assets/icons/user-icon-b.svg',
+        label: 'إدارةالأطباء',
+        icon: 'assets/icons/doctors-management-w.svg',
+        activeIcon: 'assets/icons/doctors-management-b.svg',
         route: AppRoutes.doctors.root,
         requiredPermissions: [PERMISSIONS.PATIENTS.ADD],
       },
-      
-      
+
+
     ],
   },
 
@@ -51,8 +51,8 @@ export const MENU_CONFIG: MenuCategory[] = [
       },
       {
         label: 'إدارة الخدمات',
-        icon: 'assets/icons/user-icon-w.svg',
-        activeIcon: 'assets/icons/user-icon-b.svg',
+        icon: 'assets/icons/services-w.svg',
+        activeIcon: 'assets/icons/services-b.svg',
         route: AppRoutes.services.root,
         requiredPermissions: [PERMISSIONS.PATIENTS.ADD],
       },
@@ -71,8 +71,8 @@ export const MENU_CONFIG: MenuCategory[] = [
       },
       {
         label: 'إدارة الجلسات',
-        icon: 'assets/icons/dumbbell-w.svg',
-        activeIcon: 'assets/icons/dumbbell-b.svg',
+        icon: 'assets/icons/credit-card-w.svg',
+        activeIcon: 'assets/icons/credit-card-b.svg',
         route: `${AppRoutes.therapyCards.root}`,
         requiredPermissions: [PERMISSIONS.THERAPY_CARDS.VIEW],
       },
@@ -85,8 +85,8 @@ export const MENU_CONFIG: MenuCategory[] = [
       },
       {
         label: ' إدارة الاقسام ',
-        icon: 'assets/icons/dumbbell-w.svg',
-        activeIcon: 'assets/icons/dumbbell-b.svg',
+        icon: 'assets/icons/grid-fill-w.svg',
+        activeIcon: 'assets/icons/grid-fill-b.svg',
         route: `${AppRoutes.sections.root}`,
         requiredPermissions: [PERMISSIONS.MedicalPrograms.VIEW],
       },
@@ -105,9 +105,9 @@ export const MENU_CONFIG: MenuCategory[] = [
         requiredPermissions: [PERMISSIONS.DIAGNOSIS.INDUSTRIAL.VIEW],
       },
       {
-        label: 'كروت الاصلاح الفني ',
-        icon: 'assets/icons/prosthetic-foot-w.svg',
-        activeIcon: 'assets/icons/prosthetic-foot-b.svg',
+        label: 'كروت اصلاح فني ',
+        icon: 'assets/icons/credit-card-w.svg',
+        activeIcon: 'assets/icons/credit-card-b.svg',
         route: `${AppRoutes.repairCards.root}`,
         requiredPermissions: [PERMISSIONS.THERAPY_CARDS.VIEW],
       },
@@ -121,15 +121,15 @@ export const MENU_CONFIG: MenuCategory[] = [
 
       {
         label: 'جدولة المواعيد',
-        icon: 'assets/icons/prosthetic-foot-w.svg',
-        activeIcon: 'assets/icons/prosthetic-foot-b.svg',
+        icon: 'assets/icons/appointment-w.svg',
+        activeIcon: 'assets/icons/appointment-b.svg',
         route: `${AppRoutes.Appointment.root}/${AppRoutes.Appointment.waaitngPatients}`,
         requiredPermissions: [PERMISSIONS.Appointment.VIEW],
       },
-       {
+      {
         label: 'إدارة المواعيد',
-        icon: 'assets/icons/prosthetic-foot-w.svg',
-        activeIcon: 'assets/icons/prosthetic-foot-b.svg',
+        icon: 'assets/icons/appointment-w.svg',
+        activeIcon: 'assets/icons/appointment-b.svg',
         route: `${AppRoutes.Appointment.root}/${AppRoutes.Appointment.manage}`,
         requiredPermissions: [PERMISSIONS.Appointment.VIEW],
       },
@@ -142,15 +142,15 @@ export const MENU_CONFIG: MenuCategory[] = [
     items: [
       {
         label: 'إدارة الحاسابات',
-        icon: 'assets/icons/prosthetic-foot-w.svg',
-        activeIcon: 'assets/icons/prosthetic-foot-b.svg',
+        icon: 'assets/icons/file-ruled-w.svg',
+        activeIcon: 'assets/icons/file-ruled-b.svg',
         route: AppRoutes.finance.root,
         requiredPermissions: [PERMISSIONS.PAYMENTS.VIEW],
       },
       {
         label: 'بطائق المرضى',
-        icon: 'assets/icons/user-icon-w.svg',
-        activeIcon: 'assets/icons/user-icon-b.svg',
+        icon: 'assets/icons/card-w.svg',
+        activeIcon: 'assets/icons/card-b.svg',
         route: AppRoutes.patientCards.disabled.root,
         requiredPermissions: [PERMISSIONS.PATIENTS.ADD],
       },
