838c6d7df207b039c21ed14f69763b697f1e1365
Youssef Al-Hammadi <youssefesmailalhammadi90@gmail.com>
2026-01-17 21:09:29 +0300

    design therpay card reports ^_^

--- Alatraf.Managment.system/src/app/features/Reports/pages/therpay-diagnosis-reports/therpay-diagnosis-reports.component.ts
+++ Alatraf.Managment.system/src/app/features/Reports/pages/therpay-diagnosis-reports/therpay-diagnosis-reports.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MultiSelectComponent } from "../../../../shared/components/multi-select/multi-select.component";
 
 @Component({
   selector: 'app-therpay-diagnosis-reports',
-  imports: [],
+  imports: [MultiSelectComponent],
   templateUrl: './therpay-diagnosis-reports.component.html',
   styleUrl: './therpay-diagnosis-reports.component.css'
 })
